a106ebb8d1cfaf8ca386c9ad0020d5706f2b9ab0
Pulkit Jasti <pulkit.jasti@gmail.com>
2021-04-11 17:05:32 +0530

    signup page completed

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'assets/scss/black-dashboard-react.scss';
+import 'assets/demo/demo.css';
+import 'assets/css/nucleo-icons.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+import Signup from './pages/signup/Signup';
+
+ReactDOM.render(<Signup />, document.getElementById('root'));
+
+{
+	/* <BackgroundColorWrapper>
+					<BrowserRouter>
+						<Switch>
+							<Route path='/admin' render={props => <AdminLayout {...props} />} />
+							<Route path='/rtl' render={props => <RTLLayout {...props} />} />
+							<Redirect from='/' to='/admin/dashboard' />
+						</Switch>
+					</BrowserRouter>
+				</BackgroundColorWrapper> */
+}
